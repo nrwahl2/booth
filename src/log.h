a6c2b77884c05e58030572f8b13844cbca7de224
Reid Wahl <nrwahl@protonmail.com>
2025-12-06 18:03:53 -0800

    Refactor: Take ticket argument to tk_cl_log()
    And rename to booth__log_ticket(). The goal is to avoid implicitly
    relying on a tk variable in the scope of ticket logging calls. This is
    the first step.
    
    Signed-off-by: Reid Wahl <nrwahl@protonmail.com>

--- src/log.h
+++ src/log.h
@@ -45,10 +45,11 @@
 /* all tk_* macros prepend "%(tk->name): " (the caller needs to
  * have the ticket named tk!)
  */
-#define tk_cl_log(sev, fmt, args...) \
-	priv_log(sev, "%s (%s/%d/%d): " fmt, \
-	tk->name, state_to_string(tk->state), tk->current_term, term_time_left(tk), \
-	##args)
+#define booth__log_ticket(ticket, level, fmt, args...)                  \
+    priv_log(level, "%s (%s/%d/%d): " fmt, (ticket)->name,              \
+             state_to_string((ticket)->state), (ticket)->current_term,  \
+             term_time_left(ticket), ##args)
+
 #define tk_cl_log_src(sev, fmt, args...) \
 	priv_log(sev, "%s:%d: %s (%s/%d/%d): " fmt, \
 	__FUNCTION__, __LINE__, \
@@ -58,8 +59,9 @@
 #define tk_log_debug(fmt, args...)		do { \
 	if (ANYDEBUG) tk_cl_log_src(LOG_DEBUG, fmt, ##args); } \
 	while (0)
-#define tk_log_info(fmt, args...)		tk_cl_log(LOG_INFO, fmt, ##args)
-#define tk_log_warn(fmt, args...)		tk_cl_log(LOG_WARNING, fmt, ##args)
-#define tk_log_error(fmt, args...)		tk_cl_log(LOG_ERR, fmt, ##args)
+#define tk_log_info(fmt, args...) booth__log_ticket(tk, LOG_INFO, fmt, ##args)
+#define tk_log_warn(fmt, args...) booth__log_ticket(tk, LOG_WARNING, fmt,   \
+                                                    ##args)
+#define tk_log_error(fmt, args...) booth__log_ticket(tk, LOG_ERR, fmt, ##args)
 
 #endif /* _LOG_H */
